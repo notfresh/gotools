196ade90ab35a13cb8b73184a861428cb5395d05
notfreshmbp <1195056983@qq.com>
2022-08-01 19:42:46 +0800

    wait random time

--- random.go
+++ random.go
@@ -15,16 +15,3 @@ func RandomBetween(t1, t2 int64) int64 {
 	ret := int64(r2.Float64()*float64(offset)) + n1
 	return ret
 }
-
-func RandomTimeRange(t1, t2 time.Duration) time.Duration {
-	s2 := rand.NewSource(time.Now().UnixNano())
-	r2 := rand.New(s2)
-
-	n1, n2 := int64(t1), int64(t2)
-	if n1 > n2 {
-		n2, n1 = n1, n2
-	}
-	offset := n2 - n1
-	ret := int64(r2.Float64()*float64(offset)) + n1
-	return time.Duration(ret)
-}
--- timehelper.go
+++ timehelper.go
@@ -1,6 +1,9 @@
 package gotools
 
-import "time"
+import (
+	"math/rand"
+	"time"
+)
 
 const CommonTimeStr string = "2006-01-02 15:04:05"
 
@@ -13,10 +16,28 @@ func CommonTimeParse(s string) (t time.Time, e error) {
 	return
 }
 
-
 func TimeNearEqual(time1 time.Time, time2 time.Time) bool {
 	if time1.After(time2) {
 		return time1.Sub(time2) <= time.Millisecond
 	}
 	return time2.Sub(time1) <= time.Millisecond
+}
+
+func RandomTimeRange(t1, t2 time.Duration) time.Duration {
+	s2 := rand.NewSource(time.Now().UnixNano())
+	r2 := rand.New(s2)
+
+	n1, n2 := int64(t1), int64(t2)
+	if n1 > n2 {
+		n2, n1 = n1, n2
+	}
+	offset := n2 - n1
+	ret := int64(r2.Float64()*float64(offset)) + n1
+	return time.Duration(ret)
+}
+
+func WaitRandomTimeRange(t1, t2 time.Duration) {
+	ret := RandomTimeRange(t1, t2)
+	//fmt.Println("Random Sleep for ", ret)
+	time.Sleep(ret)
 }
